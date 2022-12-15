b2ae5595166b62272d71b58a6bdb781dba22d66a
Tavi Manrique <tavidanner96@gmail.com>
2022-12-15 03:40:32 -0500

    Cleaned unnecessary code

--- app/src/main/java/com/example/musictime/presentation/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/musictime/presentation/screens/login/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.example.musictime.presentation.screens.login
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
@@ -8,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import com.example.musictime.navigation.navgraph.Graph
 
 @Composable
 fun LoginScreen(
@@ -19,10 +17,6 @@ fun LoginScreen(
         contentAlignment = Alignment.Center
     ) {
         Text(
-            modifier = Modifier.clickable {
-                rootNavController.navigate(Graph.BOTTOM)
-                rootNavController.popBackStack()
-            },
             text = "LOGIN SCREEN"
         )
     }
